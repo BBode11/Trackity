2ef533b635c93b2a7a23f6b57a3806a6b4b38154
BBode11 <bode11@mail.nmc.edu>
2021-10-17 19:41:22 -0400

    Added TypeController. Changed index.cshtml.

--- Controllers/TypeController.cs
+++ Controllers/TypeController.cs
@@ -6,97 +6,93 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Trackity.Models;
+using Type = Trackity.Models.Type;
 
 namespace Trackity.Controllers
 {
-    public class ExpenseController : Controller
+    public class TypeController : Controller
     {
         private readonly ExpenseContext _context;
 
-        public ExpenseController(ExpenseContext context)
+        public TypeController(ExpenseContext context)
         {
             _context = context;
         }
 
-        // GET: Expense
+        // GET: Type
         public async Task<IActionResult> Index()
         {
-
-            return View(await _context.Expenses.ToListAsync());
+            ViewBag.Types = _context.Types.OrderBy(t => t.Name).ToList();
+            return View(await _context.Types.ToListAsync());
         }
 
-        // GET: Expense/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Type/Details/5
+        public async Task<IActionResult> Details(string id)
         {
             if (id == null)
             {
                 return NotFound();
             }
             ViewBag.Types = _context.Types.OrderBy(t => t.Name).ToList();
-            var expense = await _context.Expenses
-                .FirstOrDefaultAsync(m => m.ExpenseId == id);
-            if (expense == null)
+            var @type = await _context.Types
+                .FirstOrDefaultAsync(m => m.TypeId == id);
+            if (@type == null)
             {
                 return NotFound();
             }
 
-            return View(expense);
+            return View(@type);
         }
 
-        // GET: Expense/Create
-        public IActionResult Create()
+        // GET: Type/Create
+        public IActionResult Create(int id)
         {
-            ViewBag.Action = "Add";
+            ViewBag.Action = "Edit";
             ViewBag.Types = _context.Types.OrderBy(t => t.Name).ToList();
-            return View("Edit", new Expense());
+            var expense = _context.Expenses.Find(id);
+            return View();
         }
 
-        // POST: Expense/Create
+        // POST: Type/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ExpenseId,Date,Name,Cost")] Expense expense)
+        public async Task<IActionResult> Create([Bind("TypeId,Name")] Type @type)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(expense);
+                _context.Add(@type);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(expense);
+            return View(@type);
         }
 
-        // GET: Expense/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        // GET: Type/Edit/5
+        public async Task<IActionResult> Edit(string id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-     
-            var expense = await _context.Expenses.FindAsync(id);
-            if (expense == null)
+
+            var @type = await _context.Types.FindAsync(id);
+            if (@type == null)
             {
                 return NotFound();
             }
-            else
-            {
-                ViewBag.Action = (expense.ExpenseId == 0) ? "Add": "Edit";
-                ViewBag.Types = _context.Types.OrderBy(t => t.Name).ToList();
-                var expenses = _context.Expenses.Find(id);
-            }
-            return View(expense);
+            return View(@type);
         }
 
-        // POST: Expense/Edit/5
+        // POST: Type/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ExpenseId,Date,Name,Cost")] Expense expense)
+        public async Task<IActionResult> Edit(string id, [Bind("TypeId,Name")] Type @type)
         {
-            if (id != expense.ExpenseId)
+            if (id != @type.TypeId)
             {
                 return NotFound();
             }
@@ -105,12 +101,12 @@ namespace Trackity.Controllers
             {
                 try
                 {
-                    _context.Update(expense);
+                    _context.Update(@type);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ExpenseExists(expense.ExpenseId))
+                    if (!TypeExists(@type.TypeId))
                     {
                         return NotFound();
                     }
@@ -121,41 +117,41 @@ namespace Trackity.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(expense);
+            return View(@type);
         }
 
-        // GET: Expense/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // GET: Type/Delete/5
+        public async Task<IActionResult> Delete(string id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-            
-            var expense = await _context.Expenses
-                .FirstOrDefaultAsync(m => m.ExpenseId == id);
-            if (expense == null)
+
+            var @type = await _context.Types
+                .FirstOrDefaultAsync(m => m.TypeId == id);
+            if (@type == null)
             {
                 return NotFound();
             }
 
-            return View(expense);
+            return View(@type);
         }
 
-        // POST: Expense/Delete/5
+        // POST: Type/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(string id)
         {
-            var expense = await _context.Expenses.FindAsync(id);
-            _context.Expenses.Remove(expense);
+            var @type = await _context.Types.FindAsync(id);
+            _context.Types.Remove(@type);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool ExpenseExists(int id)
+        private bool TypeExists(string id)
         {
-            return _context.Expenses.Any(e => e.ExpenseId == id);
+            return _context.Types.Any(e => e.TypeId == id);
         }
     }
 }
